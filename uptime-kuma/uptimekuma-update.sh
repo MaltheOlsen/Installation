ad6fa2729003d7a210f1bf6659c45cce689a5ec4
MaltheOlsen <159198406+MaltheOlsen@users.noreply.github.com>
2025-06-04 15:03:21 +0200

    Create uptimekuma-update.sh

---
+++ uptime-kuma/uptimekuma-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd <uptime-kuma-directory>
+
+# Update from git
+git fetch --all
+git checkout 1.23.16 --force
+
+# Install dependencies and prebuilt
+npm install --production
+npm run download-dist
+
+# Restart
+pm2 restart uptime-kuma
