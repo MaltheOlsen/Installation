f9bcc8c45f29f17cc51e62ae3399a93bf0ab7c5f
MaltheOlsen <159198406+MaltheOlsen@users.noreply.github.com>
2025-06-04 14:20:04 +0200

    Create uptimekuma-autoinstall.sh

---
+++ uptime-kuma/uptimekuma-autoinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/louislam/uptime-kuma.git
+cd uptime-kuma
+npm run setup
+
+# Option 1. Try it
+node server/server.js
+
+# (Recommended) Option 2. Run in the background using PM2
+# Install PM2 if you don't have it:
+npm install pm2 -g && pm2 install pm2-logrotate
+
+# Start Server
+pm2 start server/server.js --name uptime-kuma
