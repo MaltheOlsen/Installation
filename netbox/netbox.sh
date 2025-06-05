561d7d2f3db49ef760d5aec60e3aaa3d50e00237
MaltheOlsen <159198406+MaltheOlsen@users.noreply.github.com>
2025-06-05 12:03:40 +0200

    Update netbox.sh

--- netbox/netbox.sh
+++ netbox/netbox.sh
@@ -1,37 +1,123 @@
+#!/bin/bash
+
 ## NETBOX ##
 sudo apt update && sudo apt upgrade -y
 
-# Install System Packages
-sudo apt install -y python3 python3-pip python3-venv python3-dev build-essential libxml2-dev libxslt1-dev libffi-dev libpq-dev libssl-dev zlib1g-dev
+## PostgreSQL Database Installation ##
+sudo apt update
+sudo apt install -y postgresql
 
-# Download a Release Archive
-mkdir /opt/netbox-4.2.8
-sudo wget https://github.com/netbox-community/netbox/archive/refs/tags/v4.2.8.tar.gz
-sudo tar -xzf v4.2.8.tar.gz -C /opt
-sudo ln -s /opt/netbox-4.2.8/ /opt/netbox
+## Database Creation ##
+sudo -u postgres psql
 
-# Create the NetBox System User
+CREATE DATABASE netbox;
+CREATE USER netbox WITH PASSWORD 'Passw0rd';
+ALTER DATABASE netbox OWNER TO netbox;
+\connect netbox;
+GRANT CREATE ON SCHEMA public TO netbox;
+\q
+
+# Test database
+# psql --username netbox --password --host localhost netbox
+
+## Redis Installation ##
+sudo apt install -y redis-server
+# Test Redis #
+# verify that your installed version of Redis is at least v4.0 - redis-server -v
+# If successful, you should receive a PONG response from the server. - redis-cli ping
+
+## NetBox Installation ##
+sudo apt install -y python3 python3-pip python3-venv python3-dev \
+build-essential libxml2-dev libxslt1-dev libffi-dev libpq-dev \
+libssl-dev zlib1g-dev
+
+sudo wget https://github.com/netbox-community/netbox/archive/refs/tags/v4.3.1.tar.gz
+sudo tar -xzf v4.3.1.tar.gz -C /opt
+sudo ln -s /opt/netbox-4.3.1/ /opt/netbox
+
+# Create the NetBox System User #
 sudo adduser --system --group netbox
 sudo chown --recursive netbox /opt/netbox/netbox/media/
 sudo chown --recursive netbox /opt/netbox/netbox/reports/
 sudo chown --recursive netbox /opt/netbox/netbox/scripts/
 
-# Configuration
+# netbox Configuration #
 cd /opt/netbox/netbox/netbox/
 sudo cp configuration_example.py configuration.py
 
-## Edit the following lines in configuration.py
-#    ALLOWED_HOSTS - ALLOWED_HOSTS = ['netbox.example.com', '192.0.2.123']
-#    DATABASE - PostgreSQL oplysninger
-#    REDIS - Tasks and Caching database need a unique ID, eks. 0 for tasks and 1 for caching
-#    SECRET_KEY - Generate a key by executing - python3 ../generate_secret_key.py
+# ALLOWED_HOSTS #
+sed -i 's/^ALLOWED_HOSTS*/ALLOWED_HOSTS = ['*']/' /opt/netbox/netbox/netbox/configuration.py
 
-# Create a Super User/Web User
+# DATABASES #
+sed -i "/^DATABASES = {/,/^}$/c\\
+DATABASES = {\\
+    'default': {\\
+        'NAME': 'netbox',\\
+        'USER': 'netbox',\\
+        'PASSWORD': 'DIT_NYE_PASSWORD',\\
+        'HOST': 'localhost',\\
+        'PORT': '',\\
+        'CONN_MAX_AGE': 300,\\
+    }\\
+}" /opt/netbox/netbox/netbox/configuration.py
+
+# REDIS #
+sed -i '/^REDIS = {/,/^}$/c\
+REDIS = {\
+    '\''tasks'\'': {\
+        '\''HOST'\'': '\''localhost'\'',\
+        '\''PORT'\'': 6379,\
+        '\''PASSWORD'\'': '\'''\'',\
+        '\''DATABASE'\'': 0,\
+        '\''SSL'\'': False,\
+    },\
+    '\''caching'\'': {\
+        '\''HOST'\'': '\''localhost'\'',\
+        '\''PORT'\'': 6379,\
+        '\''PASSWORD'\'': '\'''\'',\
+        '\''DATABASE'\'': 1,\
+        '\''SSL'\'': False,\
+    }\
+}' /opt/netbox/netbox/netbox/configuration.py
+
+# SECRET_KEY #
+python3 ../generate_secret_key.py
+
+# Run the Upgrade Script #
+deactivate
+
+sudo /opt/netbox/upgrade.sh
+sudo PYTHON=/usr/bin/python3.10 /opt/netbox/upgrade.sh
+
+# Create a Super User #
 source /opt/netbox/venv/bin/activate
-
 cd /opt/netbox/netbox
 python3 manage.py createsuperuser
-
-# Schedule the Housekeeping Task
-#NetBox includes a housekeeping management command that handles some recurring cleanup tasks, such as clearing out old sessions and expired change records.
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+# Housekeeping Task #
 sudo ln -s /opt/netbox/contrib/netbox-housekeeping.sh /etc/cron.daily/netbox-housekeeping
+
+# Test the Application #
+# python3 manage.py runserver 0.0.0.0:8000 --insecure
+
+## Gunicorn ##
+# Configuration #
+sudo cp /opt/netbox/contrib/gunicorn.py /opt/netbox/gunicorn.py
+
+# systemd Setup #
+sudo cp -v /opt/netbox/contrib/*.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now netbox netbox-rq
+# verify it is running - systemctl status netbox.service
+
+## Obtain an SSL Certificate ##
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+-keyout /etc/ssl/private/netbox.key \
+-out /etc/ssl/certs/netbox.crt
+
+## HTTP Server Installation NGINX ##
+sudo apt install -y nginx
+sudo cp /opt/netbox/contrib/nginx.conf /etc/nginx/sites-available/netbox
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/netbox /etc/nginx/sites-enabled/netbox
+sudo systemctl restart nginx
